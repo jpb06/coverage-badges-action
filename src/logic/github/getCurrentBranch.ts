4e2de34cee1aeddd59f409e8066b7c9905cd3741
jpb06 <jp.bois.06@outlook.fr>
2024-05-25 12:45:45 +0200

    chore: adding event logs when current branch is unknown (#7)

--- src/logic/github/getCurrentBranch.ts
+++ src/logic/github/getCurrentBranch.ts
@@ -1,16 +1,23 @@
 import { info } from '@actions/core';
 
+const isEmpty = (value: string | undefined) =>
+  value === undefined || value === 'undefined' || value === '';
+
 export const getCurrentBranch = (): string => {
   let currentBranch = process.env.GITHUB_HEAD_REF;
 
-  if (currentBranch === undefined || currentBranch === 'undefined') {
+  if (isEmpty(currentBranch)) {
     currentBranch = process.env.GITHUB_REF_NAME;
   }
 
-  if (currentBranch === undefined || currentBranch === 'undefined') {
-    throw new Error('Unable to get current branch from github event.');
+  if (isEmpty(currentBranch)) {
+    console.warn('🗯️ GITHUB_HEAD_REF', process.env.GITHUB_HEAD_REF);
+    console.warn('🗯️ GITHUB_REF_NAME', process.env.GITHUB_REF_NAME);
+
+    throw new Error('🚨 Unable to get current branch from github event.');
   }
 
   info(`ℹ️ Current branch is ${currentBranch}`);
-  return currentBranch;
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  return currentBranch!;
 };
