115a2b0d3b1031c278b57dc48e827118fae1f983
jpb06 <jp.bois.06@outlook.fr>
2024-06-29 12:09:30 +0200

    fix(glob paths): accept paths not starting with ./ (#16)
    * fix(glob paths): accept paths not starting with ./
    
    * zz
    
    * oopsie

--- src/logic/coverage/get-valid-paths/validate-path.test.ts
+++ src/logic/coverage/get-valid-paths/validate-path.test.ts
@@ -40,6 +40,25 @@ describe('validatePath effect', () => {
     });
   });
 
+  it('should handle glob paths not starting with ./', async () => {
+    const globPath = 'apps/**/coverage/coverage-summary.json';
+    const resolvedPath = 'apps/front/coverage/coverage-summary.json';
+
+    pathExists.mockReturnValueOnce(true as never);
+    readJson.mockResolvedValue(summaryFileMockData);
+
+    const { validatePath } = await import('./validate-path');
+
+    const result = await Effect.runPromise(
+      validatePath(globPath)(resolvedPath),
+    );
+
+    expect(result).toStrictEqual({
+      path: 'apps/front/coverage/coverage-summary.json',
+      subPath: 'front',
+    });
+  });
+
   it('should handle basic subPaths paths', async () => {
     const globPath = './apps/front/coverage/coverage-summary.json';
     const resolvedPath = 'apps/front/coverage/coverage-summary.json';
