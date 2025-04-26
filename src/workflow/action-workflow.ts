c3627d5f3c4d49006da7d284ece2b0d64b345218
jpb06 <jpb.06@outlook.com>
2025-04-26 21:17:19 +0200

    feat: switching to effect-errors

--- src/workflow/action-workflow.ts
+++ src/workflow/action-workflow.ts
@@ -1,5 +1,6 @@
 import { NodeFileSystem } from '@effect/platform-node';
 import { Effect, Layer, pipe } from 'effect';
+import { runPromise } from 'effect-errors';
 
 import { GithubActionsLayerLive } from '@effects/deps/github-actions';
 import { LoggerConsoleLive } from '@effects/deps/logger';
@@ -8,7 +9,7 @@ import { collectErrorDetails } from '@effects/errors';
 import { mainTask } from './main-task.js';
 
 export const actionWorkflow = () =>
-  Effect.runPromise(
+  runPromise(
     pipe(
       mainTask(),
       Effect.sandbox,
